4bd933d7e4dca2abd9e1b1abb3b4a211694990c5
curzodo <curzodo@proton.me>
2023-03-04 15:48:24 +0000

    Update

--- go.mod
+++ go.mod
@@ -3,33 +3,34 @@ module github.com/curzodo/pandora
 go 1.19
 
 require (
-	github.com/curzodo/perlin v0.1.1
-	github.com/df-mc/dragonfly v0.9.2
+	github.com/df-mc/dragonfly v0.9.3
+	gonum.org/v1/gonum v0.12.0
 )
 
 require (
 	github.com/brentp/intintmap v0.0.0-20190211203843-30dc0ade9af9 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/curzodo/perlin v0.1.2-0.20230226203605-ab8d704166e7 // indirect
 	github.com/df-mc/atomic v1.10.0 // indirect
 	github.com/df-mc/goleveldb v1.1.9 // indirect
 	github.com/go-gl/mathgl v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/klauspost/compress v1.16.0 // indirect
 	github.com/muhammadmuzzammil1998/jsonc v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/sandertv/go-raknet v1.12.0 // indirect
-	github.com/sandertv/gophertunnel v1.27.2 // indirect
+	github.com/sandertv/gophertunnel v1.27.4 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/exp v0.0.0-20230206171751-46f607a40771 // indirect
-	golang.org/x/image v0.3.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/oauth2 v0.4.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
+	golang.org/x/image v0.5.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/oauth2 v0.5.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
--- pandora.go
+++ pandora.go
@@ -3,10 +3,12 @@ package main
 import (
     "github.com/curzodo/perlin"
     "github.com/df-mc/dragonfly/server"
+    "github.com/df-mc/dragonfly/server/player"
     "github.com/df-mc/dragonfly/server/block"
     "github.com/df-mc/dragonfly/server/world"
     "github.com/df-mc/dragonfly/server/world/chunk"
-    // "math"
+    "gonum.org/v1/gonum/interp"
+    "math/rand"
 )
 
 func main() {
@@ -22,7 +24,9 @@ func main() {
 	srv.CloseOnProgramEnd()
 
 	srv.Listen()
-	for srv.Accept(nil) {
+	for srv.Accept(func(p *player.Player) {
+        // p.Teleport([3]float64{10000, 64, 10000})
+    }) {
 	}
 }
 
@@ -35,6 +39,9 @@ func NewGenerator(seed int64) world.Generator {
 }
 
 var grass = world.BlockRuntimeID(block.Grass{})
+var log = world.BlockRuntimeID(block.Log{})
+var leaves = world.BlockRuntimeID(block.Leaves{})
+var dirt = world.BlockRuntimeID(block.Dirt{})
 var stone = world.BlockRuntimeID(block.Stone{})
 var water = world.BlockRuntimeID(block.Water{
     Still: true,
@@ -42,28 +49,40 @@ var water = world.BlockRuntimeID(block.Water{
     Falling: true,
 })
 
+var xs = []float64{-2.0, 0.0, 2.0}
+var ys = []float64{0, 3, 6}
+var pc interp.ClampedCubic
+var _ = pc.Fit(xs, ys)
+
 func (g Generator) GenerateChunk(pos world.ChunkPos, chunk *chunk.Chunk) {
     for x := 0; x < 16; x++ {
-        for z := 0; z < 16; z++ {
-            worldx := (float64(x + 15 * int(pos[0])) + 0.01) + 10000
-            worldz := (float64(z + 15 * int(pos[1])) + 0.01) + 10000
-            
-            spread := 500.0
-            amplitude := 20.0
-            octaves := 8
-            persistence := 1.0
-            lacunarity := 2.0
+            for z := 0; z < 16; z++ {
+                worldx := float64(x + 15 * int(pos[0])) + 10000.01
+                worldz := float64(z + 15 * int(pos[1])) + 10000.01
 
-            var noise float64
-            for i := 0; i < octaves; i++ {
-                noise += amplitude * (g.Noise2D(worldx/spread, worldz/spread) + 1)
-                amplitude *= persistence
-                spread *= 1/lacunarity
+                spread := 40.0
+                octaves := 3
+                amplitude := 2.00
+                persistence := 0.5
+                lacunarity := 2.0
+
+                noise := 0.0
+
+                for i := 0; i < octaves; i++ {
+                    noise += amplitude * g.Noise2D(worldx/spread, worldz/spread)
+                    amplitude *= persistence
+                    spread *= 1/lacunarity
+                }
+
+                y := pc.Predict(noise)
+
+                chunk.SetBlock(uint8(x), int16(y), uint8(z), 0, grass)
+
+                treeNoise := g.Noise2D(worldx/25, worldz/25)
+
+                // Spawn tree
+                if treeNoise > 0.15 && rand.Intn(100) == 0 {
+                }
             }
-
-            chunk.SetBlock(uint8(x), int16(noise)-1, uint8(z), 0, stone)
-            chunk.SetBlock(uint8(x), int16(noise), uint8(z), 0, stone)
-            chunk.SetBlock(uint8(x), int16(noise)+1, uint8(z), 0, grass)
-        }
     }
 }
